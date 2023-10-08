e003df8518cc02d4632c59d3b3be65ffcd527bb3
luxiangfei <129012967+luxiangfei@users.noreply.github.com>
2023-10-08 18:40:12 -0400

    Add files via upload

---
+++ COMP229-assignment1-master/COMP229-assignment1-master/public/Scripts/app.js
@@ -0,0 +1,13 @@
+//File name: app.js, Student Name: Wan Wan, Student ID: 301326973, Date: 4th Feb 2023
+
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
