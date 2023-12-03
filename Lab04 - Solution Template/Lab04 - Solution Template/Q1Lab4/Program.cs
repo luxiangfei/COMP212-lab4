882bd2b4a5365e7bb984f7a96ee55e03a8d942ef
luxiangfei <129012967+luxiangfei@users.noreply.github.com>
2023-12-03 18:58:04 -0500

    Add files via upload

---
+++ Lab04 - Solution Template/Lab04 - Solution Template/Q1Lab4/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Question 01 - Lab 04");
+
+// Invokes methods
+Question1_1();
+Question1_2();
+Question1_3();
+Question1_4();
+Question1_5();
+Question1_6();
+
+
+// 1.1 List the names of the countries in alphabetical order [0.5 mark]
+void Question1_1()
+{
+
+}
+
+// 1.2 List the names of the countries in descending order of number of resources [0.5 mark]
+void Question1_2()
+{
+
+}
+
+// 1.3 List the names of the countries that shares a border with Argentina [0.5 mark]
+void Question1_3()
+{
+
+}
+
+// 1.4 List the names of the countries that has more than 10,000,000 population [0.5 mark]
+void Question1_4()
+{
+
+}
+
+// 1.5 List the country with highest population [1 mark]
+void Question1_5()
+{
+
+}
+
+// 1.6 List all the religion in south America in dictionary order [1 mark]
+void Question1_6()
+{
+
+}
---
+++ Lab04 - Solution Template/Lab04 - Solution Template/Q2Lab4/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Question 02 - Lab 04");
+
+// Invokes methods
+Question2_1();
+Question2_2();
+Question2_3();
+
+//1.Get a list of all the titles and the authors who wrote them. Sort the results by title. [2 marks]
+void Question2_1()
+{
+
+}
+
+//2.Get a list of all the titles and the authors who wrote them. Sort the results by title.  Each title sort the authors alphabetically by last name, then first name[4 marks]
+void Question2_2()
+{
+
+}
+
+//3.Get a list of all the authors grouped by title, sorted by title; for a given title sort the author names alphabetically by last name then first name.[4 marks]
+void Question2_3()
+{
+
+}
